0e1c1e3205224c7bd63d7b067f564960dd16304f
Sergio [Ɐ] <subbotinsergiy1@gmail.com>
2025-11-25 14:26:10 +0100

    Improve RadixSort portability

--- radix.c
+++ radix.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "lqueue.h"
 
 void RadixSort(char* A[],int n,int k){
-     char* B[10][n];
+     char*** B;
      int pocB[10];
-     int x,y;
-     int i,j;
+     int x;
+     int y;
+     int i;
+     int j;
+     B=(char***)malloc(10*sizeof(char**));
+     if(!B)return;
+     for(i=0;i<10;i++){
+         B[i]=(char**)malloc((size_t)n*sizeof(char*));
+         if(!B[i]){
+             while(i>0){
+                 free(B[--i]);
+             }
+             free(B);
+             return;
+         }
+     }
      for(i=0;i<10;i++)pocB[i]=0;
      for(j=k-1;j>=0;j--){
           for(i=0;i<n;i++){
              x=A[i][j]-'0';
+             if(x<0||x>9)x=0;
              B[x][pocB[x]++]=A[i];
              }
           for(y=0,x=0;x<10;x++){
@@ -18,10 +35,12 @@ void RadixSort(char* A[],int n,int k){
               pocB[x]=0;
               }
      }
+     for(i=0;i<10;i++)free(B[i]);
+     free(B);
 }
 
 void RadixSortQueue(char* A[],int n,int k){
-    int x,y;
+    int x;
     int i,j;
     LQueue q;
     LQueue B[10];
@@ -30,11 +49,33 @@ void RadixSortQueue(char* A[],int n,int k){
     for(i=0;i<10;i++)B[i]=CreateQueue();
     for(i=0;i<n;i++) Enqueue(A[i],q);
 
-    /* DOPLNIT UTRIEDENIE */
+    for(j=k-1;j>=0;j--){
+        while(!IsEmptyQueue(q)){
+            temp=FrontAndDequeue(q);
+            x=temp[j]-'0';
+            if(x<0||x>9) x=0;
+            Enqueue(temp,B[x]);
+        }
+        for(x=0;x<10;x++){
+            while(!IsEmptyQueue(B[x])){
+                temp=FrontAndDequeue(B[x]);
+                Enqueue(temp,q);
+            }
+        }
+    }
 
-    i=0;    
+    i=0;
     while(!IsEmptyQueue(q)){
         temp=FrontAndDequeue(q);
         A[i++]=temp;
     }
+    for(i=0;i<10;i++)RemoveQueue(&B[i]);
+    RemoveQueue(&q);
+}
+
+void MyPrintf(const char *format,...){
+    va_list ap;
+    va_start(ap,format);
+    vprintf(format,ap);
+    va_end(ap);
 }
